e601c425607f812d6b73b6e3cf128eb5745eb6e7
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-11-23 11:58:56 +0000

    [Finish]2-5

---
+++ score.rb
@@ -0,0 +1,7 @@
+score = 70
+if (score >= 50 || score<= 100) && score >= 80
+  puts "得点は50点以上または100点以下で、かつ80点以上です"
+end
+if score >=50 || (score <= 100 && score >= 80)
+  puts "得点は50点以上または、80点以上100点以下です"
+end
